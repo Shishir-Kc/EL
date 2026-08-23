09fe66423d418e95899b04556a089dcde53088cd
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-08-23 15:00:05 +0545

    feat: include footer in app layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Features from './components/Features';
 import Terminal from './components/Terminal';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
-    <>
-      <main>
-        <Hero />
-        <About />
-        <Features />
-        <Terminal />
-      </main>
-    </>
+    <main>
+      <Hero />
+      <About />
+      <Features />
+      <Terminal />
+      <Footer />
+    </main>
   );
 }
